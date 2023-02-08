4a7fbfa543b7dc5329ec0deac9bb20ca715a8ed4
Belzie <dinh.belinda@gmail.com>
2023-02-09 10:42:20 +1100

    installed axios  and dictionary api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <header>
+          <h1>Dictionary</h1>
+        </header>
         <main>
           <Dictionary />
         </main>
