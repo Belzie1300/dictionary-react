84bf3667145c96917db895153dd427cb753d3563
Belzie <dinh.belinda@gmail.com>
2023-02-08 15:31:55 +1100

    Added Dictionary component  to create search bar via form and useState to collect data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
-  return <div className="App">Hello World</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Belinda </footer>
+      </div>
+    </div>
+  );
 }
-
-export default App;
