0ca43e2b9da3f039a6dfa1b03ef7e09f33c748a3
Belzie <dinh.belinda@gmail.com>
2023-02-14 12:12:53 +1100

    created Meaning component

---
+++ src/Meaning.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Meaning(props) {
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h2>{props.meaning.partOfSpeech}</h2>
+      <p>{props.meaning.definition}</p>
+    </div>
+  );
+}
