a5454665b69196b0edd5967ae61d7e893385a03b
Belzie <dinh.belinda@gmail.com>
2023-03-03 12:23:13 +1100

    tidy up warnings

--- src/Photos.js
+++ src/Photos.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Dictionary.css";
 
 export default function Photos(props) {
-  console.log(props.photos);
   if (props.photos) {
     return (
       <section className="Photos">
@@ -10,8 +9,12 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
-                <a href={photo.src.original} target="_blank">
-                  <img src={photo.src.landscape} className="img-fluid"></img>
+                <a href={photo.src.original} rel="noreferrer" target="_blank">
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt="Definition"
+                  ></img>
                 </a>
               </div>
             );
